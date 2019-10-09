325099e18a46ace68337e96124bf4fd4f524f1c0
tnaz <tullah.nazari@gmail.com>
2019-10-09 17:53:24 -0500

    converting random to function

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,11 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void rollTheDie() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,8 +40,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  rollTheDie();
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -46,8 +50,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  rollTheDie();
                 });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
